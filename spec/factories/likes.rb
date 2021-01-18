4617b5397bef20a053143d5c50bd53cba0dee4d1
reohashimoto <doribler@gmail.com>
2021-01-18 13:38:43 +0900

    ビューの調整

--- spec/factories/likes.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :like do
-    match { nil }
-    user { nil }
-  end
-end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe Match, type: :model do
   before do
     @match = FactoryBot.build(:match)
-    @match.home_team_img = fixture_file_upload('app/assets/images/test_image.png')
-    @match.away_team_img = fixture_file_upload('app/assets/images/test_image.png')
+    @match.home_team_image = fixture_file_upload('app/assets/images/test_image.png')
+    @match.away_team_image = fixture_file_upload('app/assets/images/test_image.png')
   end
+
     describe '試合投稿機能' do
       context "試合投稿できる場合" do
         it "全ての情報が正しく入力されていれば投稿できる" do
@@ -13,6 +14,17 @@ RSpec.describe Match, type: :model do
         end
       end
       context "試合投稿できない場合" do
+        it "リーグか大会名がないと投稿できない" do
+          @match.league = ""
+          @match.valid?
+          expect(@match.errors.full_messages).to include("League can't be blank")
+        end
+        
+        it "開催場所がないと投稿できない" do
+          @match.place = ""
+          @match.valid?
+          expect(@match.errors.full_messages).to include("Place can't be blank")
+        end
         it "ホームチーム名がないと投稿できない" do
           @match.home_team_name = ""
           @match.valid?
@@ -26,15 +38,15 @@ RSpec.describe Match, type: :model do
         end
 
         it "ホームチーム画像がないと投稿できない" do
-          @match.home_team_img = ""
+          @match.home_team_image = nil
           @match.valid?
-          expect(@match.errors.full_messages).to include("Home team img can't be blank")
+          expect(@match.errors.full_messages).to include("Home team image can't be blank")
         end
 
         it "アウェイチーム画像がないと投稿できない" do
-          @match.away_team_img = ""
+          @match.away_team_image = nil
           @match.valid?
-          expect(@match.errors.full_messages).to include("Away team img can't be blank")
+          expect(@match.errors.full_messages).to include("Away team image can't be blank")
         end
 
         it "ホームチームスコアがないと投稿できない" do
@@ -42,17 +54,30 @@ RSpec.describe Match, type: :model do
           @match.valid?
           expect(@match.errors.full_messages).to include("Home team score can't be blank")
         end
+        it "ホームチームスコアが数字でないと投稿できない" do
+          @match.home_team_score = "a"
+          @match.valid?
+          expect(@match.errors.full_messages).to include("Home team score is not a number")
+        end
 
         it "アウェイチームスコアがないと投稿できない" do
           @match.away_team_score = ""
           @match.valid?
           expect(@match.errors.full_messages).to include("Away team score can't be blank")
         end
+
+        it "アウェイチームのスコアが数字でない登録できない" do
+          @match.away_team_score = "a"
+          @match.valid?
+          expect(@match.errors.full_messages).to include("Away team score is not a number")
+        end
+
         it "日付がないと投稿できない" do
           @match.date = nil
           @match.valid?
           expect(@match.errors.full_messages).to include("Date can't be blank")
         end
+
       end
     end
 end
